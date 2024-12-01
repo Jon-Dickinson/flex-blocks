3302d1825770f6a62c0b0963e45c6c431042afce
Jonathan Dickinson <jonathan.dickinson@libertyhealth.net>
2024-12-01 12:48:48 +0200

    added examples

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,15 @@ import '../css/flex-blocks.css';
 import '../css/custom.css';
 
 import flexDiagram from '../images/flex-diagram.png';
+import flexBlock1 from '../images/flex-block-1.png';
+import flexBlock2 from '../images/flex-block-2.png';
+import flexBlock3 from '../images/flex-block-3.png';
 
 const img = document.createElement('img');
 img.src = flexDiagram;
 img.alt = 'Flex Diagram';
-document.body.appendChild(img);
+
+
+
+
+// document.body.appendChild(img);
